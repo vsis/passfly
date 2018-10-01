7b77edf8d97cd195646629fe2d46c5568603cfa1
Roddy González <roddy.gonzalez.89@gmail.com>
2018-10-01 17:14:28 -0300

    Fixed style and added "OK" message in tests program

--- src/tests.c
+++ src/tests.c
@@ -15,11 +15,19 @@ unsigned char run_test(void (* test)(char* ), char * test_name){
     }
     return 0;
 }
+
 int main (int argc, char * argv[]){
     int failed_count = 0;
+
+    /*************************************************************************/
     //put here all tests
     failed_count += run_test(test_prnga_wiki, "test_prnga_wiki");
     failed_count += run_test(test_prnga_rfc6229, "test_prnga_rfc6229");
+    //end of tests
+    /*************************************************************************/
 
+    if (! failed_count){
+        printf("All tests OK\n");
+    }
     return failed_count;
 }
